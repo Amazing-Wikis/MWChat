4fa78cf7fd32eef532c6be07235ec31d4af43ca1
Kevin Zhang <37385853+AmazingWikis@users.noreply.github.com>
2023-01-22 21:36:50 -0500

    adding special pages
    probably needs more refinement for usability

--- sql/chat_inviteOnly.sql
+++ sql/chat_inviteOnly.sql
@@ -2,8 +2,6 @@ CREATE TABLE /*_*/chat_inviteOnly (
 cio_room_id int(10) NOT NULL PRIMARY KEY,
 cio_room_name text,
 cio_creator_id, -- foreign key to actor table
-cio_status int(10), -- 0 for active, 1 for archive
+cio_status int(10), -- 1 for active, 0 for archive
 ) /*$wgDBTableOptions*/;
-
-
-
+INSERT INTO chat_inviteOnly VALUES (1, moderation, 1, 1);
