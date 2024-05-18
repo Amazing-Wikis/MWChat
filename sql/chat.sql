32984df7fe9f2f679c39f26e27e241506792a32b
Amazing Wikis <37385853+AmazingWikis@users.noreply.github.com>
2024-05-18 17:55:58 -0400

    Update chat.sql
    modernize

--- sql/chat.sql
+++ sql/chat.sql
@@ -1,9 +1,10 @@
 CREATE TABLE /*_*/chat (
-  chat_timestamp bigint NOT NULL PRIMARY KEY,
-  chat_message text,
-  chat_user_id int(10), -- replace by actor.actor_id via foreign key
-  chat_actor_id bigint, -- foreign key to actor.actor_id
-  chat_message_visibility int(10), -- visibility as public [0] or private [1]
-  chat_channel_type bigint, -- 0 is public, 1 is mods, 2+ is invite only channel id
-  chat_to_id int(10) -- what is this for?
+  mwc_message_id bigint NOT NULL PRIMARY KEY,
+  mwc_timestamp datetime, -- timestamp of chat message 
+  mwc_user_id int(10), -- replace by mwc_actor_id
+  mwc_actor_id bigint, -- foreign key to actor.actor_id
+  mwc_message text, -- actual chat message
+  mwc_message_visibility int(10), -- visibility as public [0] or private [1]
+  mwc_channel_type bigint, -- 0 is public, 1 is mods, 2+ is invite only channel id
+  mwc_to_id int(10) -- what is this for?
 ) /*$wgDBTableOptions*/;
