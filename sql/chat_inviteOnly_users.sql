789ff7c9ec317b7e30d9f7fcdc740fba021df88f
Amazing Wikis <37385853+AmazingWikis@users.noreply.github.com>
2024-05-18 17:58:54 -0400

    Update chat_inviteOnly_users.sql

--- sql/chat_inviteOnly_users.sql
+++ sql/chat_inviteOnly_users.sql
@@ -3,5 +3,7 @@
 CREATE TABLE /*_*/mwc_invite (
 mwc_invite_id bigint NOT NULL PRIMARY KEY,
 mwc_channel_id int, -- foreign key
-mwc_invited_user -- foreign key to actor table
+mwc_invited_user bigint(20) unsigned, -- foreign key to actor.actor_id
+mwc_invite_expire datetime, -- expiration of invite
+mwc_invite_status int -- status of whether invite is expired
 ) /*$wgDBTableOptions*/;
