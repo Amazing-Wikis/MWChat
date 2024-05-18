ae871b0d73863032f414e916b5e693b16db9a8b8
Amazing Wikis <37385853+AmazingWikis@users.noreply.github.com>
2024-05-18 17:52:40 -0400

    Update chat_inviteOnly.sql

--- sql/chat_inviteOnly.sql
+++ sql/chat_inviteOnly.sql
@@ -1,7 +1,7 @@
 CREATE TABLE /*_*/mwc_invite_only (
 mwc_channel_id int(10) NOT NULL PRIMARY KEY,
 mwc_channel_name text NOT NULL,
-mwc_creator_id, -- foreign key to actor table
+mwc_creator_id, -- foreign key to actor.actor_id
 mwc_channel_status int, -- 1 for active, 0 for archive
 ) /*$wgDBTableOptions*/;
 
