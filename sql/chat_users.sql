08d51f3b4577dd2cbe5c5a16dc7f1dbea2aa0034
Amazing Wikis <37385853+AmazingWikis@users.noreply.github.com>
2024-05-18 17:54:17 -0400

    Update chat_users.sql

--- sql/chat_users.sql
+++ sql/chat_users.sql
@@ -1,7 +1,7 @@
 CREATE TABLE /*_*/chat_users (
-  cu_actor_id bigint(20) unsigned PRIMARY KEY, -- foreign key to actor.actor_id
-  cu_session_token bigint NOT NULL,
-  cu_status int(10) -- 0 for active, 1 for afk, 2 for banned
-  cu_user_id int(10), -- replace with cu_actor_id
-  cu_away binary(12), -- replace with cu_status
+  mwc_actor_id bigint(20) unsigned PRIMARY KEY, -- foreign key to actor.actor_id
+  mwc_session_token bigint NOT NULL,
+  mwc_status int(10) -- 0 for active, 1 for afk, 2 for banned
+  mwc_user_id int(10), -- replace with cu_actor_id
+  mwc_away binary(12), -- replace with cu_status
 ) /*$wgDBTableOptions*/;
