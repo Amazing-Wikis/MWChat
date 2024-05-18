631ddc4d5e71a9ce73a72474133d81ed18fc34db
Amazing Wikis <37385853+AmazingWikis@users.noreply.github.com>
2024-05-18 17:59:57 -0400

    Update chat.sql
    removing unknown field

--- sql/chat.sql
+++ sql/chat.sql
@@ -5,6 +5,5 @@ CREATE TABLE /*_*/chat (
   mwc_actor_id bigint, -- foreign key to actor.actor_id
   mwc_message text, -- actual chat message
   mwc_message_visibility int(10), -- visibility as public [0] or private [1]
-  mwc_channel_type bigint, -- 0 is public, 1 is mods, 2+ is invite only channel id
-  mwc_to_id int(10) -- what is this for?
+  mwc_channel_type bigint -- 0 is public, 1 is mods, 2+ is invite only channel id
 ) /*$wgDBTableOptions*/;
