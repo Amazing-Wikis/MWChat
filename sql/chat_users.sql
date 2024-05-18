6a8d531f6674aa23bb5edb468235b97ab23160a4
Amazing Wikis <37385853+AmazingWikis@users.noreply.github.com>
2024-05-18 17:39:12 -0400

    Update chat_users.sql
    adding user session token to allow for kick or ban

--- sql/chat_users.sql
+++ sql/chat_users.sql
@@ -1,5 +1,6 @@
 CREATE TABLE /*_*/chat_users (
   cu_user_id int(10),
   cu_timestamp binary(12),
-  cu_away binary(12)
+  cu_away binary(12),
+  cu_session_token bigint NOT NULL
 ) /*$wgDBTableOptions*/;
