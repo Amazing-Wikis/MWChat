a1cb07eb41489cf332ac52fcaa131a1198bbdd9f
Amazing Wikis <37385853+AmazingWikis@users.noreply.github.com>
2024-05-18 17:50:09 -0400

    reconfiguring chat user sql
    replacing with modern sql standard

--- sql/chat_users.sql
+++ sql/chat_users.sql
@@ -1,7 +1,7 @@
 CREATE TABLE /*_*/chat_users (
-  cu_user_id int(10),
-  cu_timestamp binary(12),
-  cu_away binary(12),
-  cu_session_token bigint NOT NULL
-  cu_status int(10) -- 0 for not sanctions, 1 for banned
+  cu_actor_id bigint(20) unsigned PRIMARY KEY, -- foreign key to actor.actor_id
+  cu_session_token bigint NOT NULL,
+  cu_status int(10) -- 0 for active, 1 for afk, 2 for banned
+  cu_user_id int(10), -- replace with cu_actor_id
+  cu_away binary(12), -- replace with cu_status
 ) /*$wgDBTableOptions*/;
