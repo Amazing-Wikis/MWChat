a01b58edb8454772e337bb2a68b8f986757efcc6
Amazing Wikis <37385853+AmazingWikis@users.noreply.github.com>
2024-05-18 17:41:07 -0400

    Update chat_users.sql
    adding a user block status

--- sql/chat_users.sql
+++ sql/chat_users.sql
@@ -3,4 +3,5 @@ CREATE TABLE /*_*/chat_users (
   cu_timestamp binary(12),
   cu_away binary(12),
   cu_session_token bigint NOT NULL
+  cu_status int(10) -- 0 for not sanctions, 1 for banned
 ) /*$wgDBTableOptions*/;
