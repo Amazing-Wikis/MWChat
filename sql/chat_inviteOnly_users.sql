2fa39107c5e9847091efebfed12ac592bc01838e
Amazing Wikis <37385853+AmazingWikis@users.noreply.github.com>
2024-05-18 17:54:00 -0400

    Update chat_inviteOnly_users.sql

--- sql/chat_inviteOnly_users.sql
+++ sql/chat_inviteOnly_users.sql
@@ -1,7 +1,7 @@
 -- all invited users per channel id, 1 user per row
 
 CREATE TABLE /*_*/mwc_invite (
-mwc_invite_id int NOT NULL PRIMARY KEY,
+mwc_invite_id bigint NOT NULL PRIMARY KEY,
 mwc_channel_id int, -- foreign key
 mwc_invited_user -- foreign key to actor table
 ) /*$wgDBTableOptions*/;
