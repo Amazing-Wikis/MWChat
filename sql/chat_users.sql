a5f851de10bb817a1faa566f15dc04de0206c1b0
Amazing Wikis <37385853+AmazingWikis@users.noreply.github.com>
2024-05-18 17:59:45 -0400

    Update chat_users.sql
    removing deprecated fields

--- sql/chat_users.sql
+++ sql/chat_users.sql
@@ -2,6 +2,4 @@ CREATE TABLE /*_*/chat_users (
   mwc_actor_id bigint(20) unsigned PRIMARY KEY, -- foreign key to actor.actor_id
   mwc_session_token bigint NOT NULL,
   mwc_status int(10) -- 0 for active, 1 for afk, 2 for banned
-  mwc_user_id int(10), -- replace with cu_actor_id
-  mwc_away binary(12), -- replace with cu_status
 ) /*$wgDBTableOptions*/;
